33a4030fcc435d3068b1da9cd4793e36c268682f
daze <leon.baiocchi@studio.unibo.it>
2022-03-24 19:18:46 +0100

    Add potentiometer

--- Assignment_1.ino
+++ Assignment_1.ino
@@ -3,6 +3,7 @@
  * - Check repeated block of code inside the super-loop.
  * - Add flags in order to delete repetition, allowing for increased performance.
  */
+#include <EnableInterrupt.h>
 #include <avr/sleep.h>
 #define RED_LED_PIN 9
 #define GREEN_LED1_PIN 7
@@ -13,9 +14,18 @@
 #define T2_BTN_PIN 3
 #define T3_BTN_PIN 12
 #define T4_BTN_PIN 13
+#define POT_PIN A0
 #define FADE_AMOUNT 0.25
 #define FADING_DELAY 1
+#define DEBOUNCE_TIME 200
 
+/*
+ * Factor by which T1 and T2 timeouts are shortened.
+ */
+unsigned int factor;
+const unsigned int lenDifficulties = 8;
+const unsigned int difficulties[lenDifficulties] = {50, 100, 200, 400, 800, 1600, 3200, 6400};
+unsigned int currentDifficulty = 0;
 /*
  * Number of leds and buttons.
  */
@@ -36,7 +46,7 @@ float redLedCurrIntensity;
 /*
  * T1 button rebound management.
  */
-unsigned long btnLastTime = 0;
+unsigned long btnLastTime = 0;  //DEFAULT
 unsigned long btnCurrentTime;
 unsigned long btnElapsed;
 /*
@@ -57,7 +67,7 @@ const unsigned int greenLedPins[len] = {GREEN_LED1_PIN, GREEN_LED2_PIN, GREEN_LE
 unsigned int currentGreenLed = 0;
 boolean bounceForward = true;
 boolean hasStoppedBouncing = false;
-unsigned long t1 = random(10000, 20000); // between 10 s and 20 s 
+unsigned long t1;
 unsigned long startTime;
 unsigned long possibleFinalTime;
 unsigned long totalElapsed = 0;
@@ -71,9 +81,14 @@ unsigned long t2 = 10000; // 10 s
 /*
  * Score and game buttons management.
  */
+const unsigned int gameBtnPins[len] = {T1_BTN_PIN, T2_BTN_PIN, T3_BTN_PIN, T4_BTN_PIN};
+int currentBtnPressed = -1;
 unsigned int score = 0;
+boolean gameOver = false;
 
 void setup() {
+  randomSeed(analogRead(1));
+  t1 = random(10000, 20000); // between 10 s and 20 s 
   redLedCurrIntensity = 0;
   redLedFadeAmount = FADE_AMOUNT;
   pinMode(RED_LED_PIN, OUTPUT);
@@ -82,7 +97,10 @@ void setup() {
   pinMode(GREEN_LED3_PIN, OUTPUT);
   pinMode(GREEN_LED4_PIN, OUTPUT);
   pinMode(T1_BTN_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(T1_BTN_PIN), startBouncingLedGame, RISING);
+  pinMode(T2_BTN_PIN, INPUT);
+  pinMode(T3_BTN_PIN, INPUT);
+  pinMode(T4_BTN_PIN, INPUT);
+  enableInterrupt(T1_BTN_PIN, startBouncingLedGame, RISING);
   Serial.begin(9600);
   Serial.println("Welcome to the Catch the Bouncing Led Ball Game. Press Key T1 to Start");  //TODO: fix duplicate message.
   Serial.flush();
@@ -111,8 +129,11 @@ void goToDeepSleep() {
   Serial.flush();
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   sleep_enable();
-  detachInterrupt(digitalPinToInterrupt(T1_BTN_PIN));
-  attachInterrupt(digitalPinToInterrupt(T1_BTN_PIN), wakeUp, RISING);
+  disableInterrupt(T1_BTN_PIN);
+  enableInterrupt(T1_BTN_PIN, wakeUp, RISING);
+  enableInterrupt(T2_BTN_PIN, wakeUp, RISING);
+  enableInterrupt(T3_BTN_PIN, wakeUp, RISING);
+  enableInterrupt(T4_BTN_PIN, wakeUp, RISING);
   sleep_mode();
 }
 
@@ -122,9 +143,9 @@ void goToDeepSleep() {
 void wakeUp() {
   Serial.println("Waking up..");
   sleep_disable();
-  detachInterrupt(digitalPinToInterrupt(T1_BTN_PIN));
+  disableInterrupt(T1_BTN_PIN);
   btnLastTime = millis();
-  attachInterrupt(digitalPinToInterrupt(T1_BTN_PIN), startBouncingLedGame, RISING);
+  enableInterrupt(T1_BTN_PIN, startBouncingLedGame, RISING);
 }
 
 // Simple utility method used to convert millis to secs.
@@ -133,6 +154,7 @@ double getCurrentTimeInSeconds() {
 }
 
 void startBouncingLedGame() {
+  Serial.println(String("Difficulty: ") + factor);
   hasStoppedBouncing = false;
   analogWrite(RED_LED_PIN, 0);
   digitalWrite(GREEN_LED1_PIN, 0);
@@ -143,7 +165,7 @@ void startBouncingLedGame() {
     btnCurrentTime = millis();
     btnElapsed = btnCurrentTime - btnLastTime;
   }
-  if ((btnElapsed > 200) || (btnLastTime == 0)) {
+  if ((btnElapsed > DEBOUNCE_TIME) || (btnLastTime == 0)) {
     Serial.println(String("btnElapsed: ") + btnElapsed);
     Serial.println(String("btnLastTime: ") + btnLastTime);
     gameStarted = true;
@@ -152,7 +174,10 @@ void startBouncingLedGame() {
     lastBounceTime = millis(); 
     startTime = lastBounceTime;
     digitalWrite(greenLedPins[currentGreenLed], 255);
-    detachInterrupt(digitalPinToInterrupt(T1_BTN_PIN));
+    disableInterrupt(T1_BTN_PIN);
+    disableInterrupt(T2_BTN_PIN);
+    disableInterrupt(T3_BTN_PIN);
+    disableInterrupt(T4_BTN_PIN);
   }
 }
 
@@ -161,6 +186,45 @@ void stopBall() {
   hasStoppedBouncing = true;
   digitalWrite(greenLedPins[currentGreenLed], 0);
   lastRecordedTime = millis();
+}
+
+void pressGameButton(int index) {
+  currentBtnPressed = index;
+  if (currentBtnPressed == currentGreenLed) {
+    score += 1;
+    hasStoppedBouncing = false;
+    lastBounceTime = millis();
+    startTime = lastBounceTime;  //Each new level timeout1 is reset.
+    currentGreenLed = 0;
+    digitalWrite(greenLedPins[currentGreenLed], 255);
+    t1 -= (t1 > factor) ? factor : 0;
+    t2 -= (t2 > factor) ? factor : 0;
+    Serial.println(String("t1: ") + t1);
+    Serial.println(String("t2: ") + t2);
+  }
+  else {
+    gameOver = true;
+  }
+}
+
+void pressGameButton1() {
+  pressGameButton(0);
+  delay(DEBOUNCE_TIME);
+}
+
+void pressGameButton2() {
+  pressGameButton(1);
+  delay(DEBOUNCE_TIME);
+}
+
+void pressGameButton3() {
+  pressGameButton(2);
+  delay(DEBOUNCE_TIME);
+}
+
+void pressGameButton4() {
+  pressGameButton(3);
+  delay(DEBOUNCE_TIME);
 }
 
 // Super loop.
@@ -175,6 +239,11 @@ void loop() {
       time0 = getCurrentTimeInSeconds();
     }
     fadingRedLed();
+    unsigned int potValue = analogRead(POT_PIN);
+    if (potValue != currentDifficulty) {
+      currentDifficulty = map(potValue, 0, 1023, 0, 8);
+      factor = difficulties[currentDifficulty];
+    }
   }
   //GAME-STAGE
   else {
@@ -206,25 +275,35 @@ void loop() {
       }
       else {
         stopBall();
+        enableInterrupt(T1_BTN_PIN, pressGameButton1, RISING);
+        enableInterrupt(T2_BTN_PIN, pressGameButton2, RISING);
+        enableInterrupt(T3_BTN_PIN, pressGameButton3, RISING);
+        enableInterrupt(T4_BTN_PIN, pressGameButton4, RISING);
       }
     }
     else {
       Serial.println("User has to press btn");
       currentTime2 = millis();
       elapsedFromLastRecorded = currentTime2 - lastRecordedTime;
-      if (elapsedFromLastRecorded >= t2) {
+      if (elapsedFromLastRecorded >= t2 || gameOver) {
         //TODO: Game Over
         Serial.println(String("Game Over. Final Score: ") + score);
+        score = 0;
         gameStarted = false;
+        gameOver = false;
         time0 = getCurrentTimeInSeconds();
         currentGreenLed = 0;
-        //attachInterrupt(digitalPinToInterrupt(T1_BTN_PIN), startBouncingLedGame, RISING);
+        currentBtnPressed = -1;
+        disableInterrupt(T1_BTN_PIN);
+        disableInterrupt(T2_BTN_PIN);
+        disableInterrupt(T3_BTN_PIN);
+        disableInterrupt(T4_BTN_PIN);
+        enableInterrupt(T1_BTN_PIN, startBouncingLedGame, RISING);
+        randomSeed(analogRead(1));
+        t1 = random(10000, 20000);
+        t2 = 10000;
       }
-      else {
-        //TODO: do nothing, waiting for button click.
-        
-        //TODO: button interrupt handler has to reset bouncing and change level (F).
-      }
+      else {}
     }
   }
 }
